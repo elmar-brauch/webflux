6003f3bf30f747b8f2421b0f46a4ec36797ee93e
Elmar Brauch <elmar.brauch@telekom.de>
2022-07-17 17:11:33 +0200

    ...

--- src/main/java/de/bsi/webflux/cache/EmployeeCacheConfiguration.java
+++ src/main/java/de/bsi/webflux/cache/EmployeeCacheConfiguration.java
@@ -1,12 +1,6 @@
 package de.bsi.webflux.cache;
 
-import java.time.Duration;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
 @Configuration
 // TODO Activate Cache for application by annotation 
@@ -15,11 +9,4 @@ public class EmployeeCacheConfiguration {
 	// TODO Create RedisCacheConfiguration bean
 	// 		with default config, TTL and Jackson serialization
 	
-	@Bean
-	public RedisCacheConfiguration cacheConfiguration() {
-		return RedisCacheConfiguration.defaultCacheConfig()
-				.entryTtl(Duration.ofMinutes(1))
-				.serializeValuesWith(SerializationPair.fromSerializer(
-						new GenericJackson2JsonRedisSerializer()));
-	}
 }
